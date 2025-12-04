519250bbab1ddd9bd5caf15f3c400c60db0ff21a
SeoHyeon <qortj1120@kookmin.ac.kr>
2025-12-04 15:24:15 -0800

    feat : 사용하지 않은 아이콘 정리

--- web/src/pages/child/ChildProfile.tsx
+++ web/src/pages/child/ChildProfile.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../../lib/supabase';
 import ChildTabNav from '../../components/ChildTabNav';
-import Icon from '../../components/Icon';
 
 interface ChildSession {
   childId: string;
